05070c7a3681d55a40d338f23299db0b092c6664
david <hirakoshinji722@gmail.com>
2021-01-16 13:12:11 +0100

    add retrofit Ulesson API service and tests

--- core/src/test/java/io/davidosemwota/core/network/responses/UlessonServiceTest.kt
+++ core/src/test/java/io/davidosemwota/core/network/responses/UlessonServiceTest.kt
@@ -1,9 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) $today.day/$today.month/2021 $today.hour24:$today.minute   David Osemwota.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.davidosemwota.core.network.responses
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
 import io.davidosemwota.core.network.UlessonService
 import io.davidosemwota.core.utils.getJson
+import java.net.HttpURLConnection
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
@@ -15,7 +39,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.net.HttpURLConnection
 
 internal class UlessonServiceTest {
 
@@ -52,9 +75,9 @@ internal class UlessonServiceTest {
         val HTTP_REQUEST = "GET"
         mockWebServer.dispatcher = object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
-                return  MockResponse()
+                return MockResponse()
                     .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody( getJson(MockResponses.list_of_subject_200) )
+                    .setBody(getJson(MockResponses.list_of_subject_200))
             }
         }
 
--- core/src/test/java/io/davidosemwota/core/util/ApiResponseUtil.kt
+++ core/src/test/java/io/davidosemwota/core/util/ApiResponseUtil.kt
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) $today.day/$today.month/2021 $today.hour24:$today.minute   David Osemwota.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.davidosemwota.core.util
 
 import io.davidosemwota.core.network.responses.Chapter
@@ -5,40 +28,40 @@ import io.davidosemwota.core.network.responses.Lesson
 import io.davidosemwota.core.network.responses.Subject
 
 val id
-    get() = listOf(22,63,456,11,71,28,32,56).random()
+    get() = listOf(22, 63, 456, 11, 71, 28, 32, 56).random()
 
 val subjectId
-    get() = listOf(22,63,456,11,71,28,32,56).random()
+    get() = listOf(22, 63, 456, 11, 71, 28, 32, 56).random()
 
 val chapterId
-    get() = listOf(22,63,456,11,71,28,32,56).random()
+    get() = listOf(22, 63, 456, 11, 71, 28, 32, 56).random()
 
 val lessonName
     get() = listOf(
-    "How to defeat your first arrancar Part I",
-    "Kido for dummies",
-    "Gotei 13 Combat Fundamentals",
-    "Katana for beginners"
-).random()
+        "How to defeat your first arrancar Part I",
+        "Kido for dummies",
+        "Gotei 13 Combat Fundamentals",
+        "Katana for beginners"
+    ).random()
 
 val icon
     get() = listOf(
-    "https://hellomedia/images/dog.png",
-    "https://hellomedia/images/cat.png",
-    "https://hellomedia/images/rat.png",
-    "https://hellomedia/images/lion.png",
-    "https://hellomedia/images/tiger.png"
-).random()
+        "https://hellomedia/images/dog.png",
+        "https://hellomedia/images/cat.png",
+        "https://hellomedia/images/rat.png",
+        "https://hellomedia/images/lion.png",
+        "https://hellomedia/images/tiger.png"
+    ).random()
 
 val mediaUrl
     get() = listOf(
-    "https://hellomedia/images/dog_dancing.mp4",
-    "https://hellomedia/images/goat_dancing.mp4",
-    "https://hellomedia/images/kido101.mp4",
-    "https://hellomedia/images/combat_statistics.mp4",
-    "https://hellomedia/images/kido.mp4",
-    "https://hellomedia/images/kido_pt2.mp4"
-).random()
+        "https://hellomedia/images/dog_dancing.mp4",
+        "https://hellomedia/images/goat_dancing.mp4",
+        "https://hellomedia/images/kido101.mp4",
+        "https://hellomedia/images/combat_statistics.mp4",
+        "https://hellomedia/images/kido.mp4",
+        "https://hellomedia/images/kido_pt2.mp4"
+    ).random()
 
 fun getLesson(): Lesson = Lesson(
     id = id,
